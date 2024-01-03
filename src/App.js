0e90c45b6a0c365b4cbbd707bec3115b52cbfebf
CodeSandbox <support@codesandbox.io>
2024-01-03 10:59:37 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Header from "./Header";
-import AddItem from "./AddItem";
+import AddItem from "./Additem";
+import SearchItem from "./SearchItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([
-    { id: 1, checked: true, item: "practice Coding" },
-    { id: 2, checked: false, item: "play Cricket" },
-    { id: 3, checked: false, item: "Read about AI" },
-  ]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("todo_list")),
+  );
+
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const addNewItem = { id, checked: false, item };
+    const listItems = [...items, addNewItem];
+    setItems(listItems);
+    localStorage.setItem("todo_list", JSON.stringify(listItems));
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
@@ -25,12 +35,25 @@ function App() {
     localStorage.setItem("todo_list", JSON.stringify(listItems));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    console.log(newItem);
+    addItem(newItem);
+    setNewItem("");
+  };
+
   return (
     <div className="App">
       <Header title="To do list" />
-      <AddItem />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter((item) => item.item.includes(search))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
